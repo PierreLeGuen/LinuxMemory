a2a24785791f09d3ff481ac1ba30c3989d692802
PierreLeGuen <pierre@leguen.fr>
2019-03-09 00:01:46 +0100

    documentation

--- LinuxMemory/include/LinuxMemory.h
+++ LinuxMemory/include/LinuxMemory.h
@@ -1,6 +1,8 @@
 #ifndef LINUXMEMORY_H
 #define LINUXMEMORY_H
 
+#include <sys/types.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -21,21 +23,20 @@ typedef struct LinuxProc_s {
 /**
  * @brief Reads data from an area of memory in a specified process. The entire area to be read must be accessible or the operation fails.
  *
- * @param hProcess : A handle to the process with memory that is being read. The handle must have PROCESS_VM_READ access to the process.
- * @param lpBaseAddress : A pointer to the base address in the specified process from which to read. Before any data transfer occurs, the system verifies that all data in the base address and memory of the specified size is accessible for read access, and if it is not accessible the function fails.
- * @param lpBuffer : A pointer to a buffer that receives the contents from the address space of the specified process.
+ * @param lpProcess : A handle to the process with memory that is being read. The handle must have PROCESS_VM_READ access to the process.
+ * @param vpBaseAddress : A pointer to the base address in the specified process from which to read. Before any data transfer occurs, the system verifies that all data in the base address and memory of the specified size is accessible for read access, and if it is not accessible the function fails.
+ * @param vpBuffer : A pointer to a buffer that receives the contents from the address space of the specified process.
  * @param nSize : The number of bytes to be read from the specified process.
  * @param lpNumberOfBytesRead : A pointer to a variable that receives the number of bytes transferred into the specified buffer. If lpNumberOfBytesRead is NULL, the parameter is ignored.
 */
-void ReadProcessMemory(LinuxProc_t lpProcess, void *vpBaseAddress, void *vpBuffer, size_t nSize,
-                       size_t *lpNumberOfBytesRead);
+void ReadProcessMemory(LinuxProc_t lpProcess, void *vpBaseAddress, void *vpBuffer, size_t nSize, size_t *lpNumberOfBytesRead);
 
 /**
  * @brief Writes data to an area of memory in a specified process. The entire area to be written to must be accessible or the operation fails.
  *
- * @param hProcess : A handle to the process memory to be modified. The handle must have PROCESS_VM_WRITE and PROCESS_VM_OPERATION access to the process.
- * @param lpBaseAddress : A pointer to the base address in the specified process to which data is written. Before data transfer occurs, the system verifies that all data in the base address and memory of the specified size is accessible for write access, and if it is not accessible, the function fails.
- * @param lpBuffer : A pointer to the buffer that contains data to be written in the address space of the specified process.
+ * @param lpProcess : A handle to the process memory to be modified. The handle must have PROCESS_VM_WRITE and PROCESS_VM_OPERATION access to the process.
+ * @param vpBaseAddress : A pointer to the base address in the specified process to which data is written. Before data transfer occurs, the system verifies that all data in the base address and memory of the specified size is accessible for write access, and if it is not accessible, the function fails.
+ * @param vpBuffer : A pointer to the buffer that contains data to be written in the address space of the specified process.
  * @param nSize : The number of bytes to be written to the specified process.
  * @param lpNumberOfBytesRead : A pointer to a variable that receives the number of bytes transferred into the specified process. This parameter is optional. If lpNumberOfBytesWritten is NULL, the parameter is ignored.
  */
@@ -65,11 +66,20 @@ void *getModuleBaseAddress(pid_t ProcessID, const char *ProcessName);
  */
 LinuxProc_t fillProcessStructbyName(const char *ProcessName);
 
-/*
- * Attach to a process, needed to write, read in his memory
+/**
+ * @brief Permits attaching to a given process
+ * 
+ * @param target A LinuxProc_t struct containing the target
+ * @return int 
  */
 int attach(LinuxProc_t target);
 
+/**
+ * @brief Permits detaching from a given process
+ * 
+ * @param target A LinuxProc_t struct containing the target
+ * @return int 
+ */
 int detach(LinuxProc_t target);
 
 #ifdef __cplusplus
